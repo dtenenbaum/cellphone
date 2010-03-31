545b862c86e8f12450012b38a46399a6d03aca23
Dan Tenenbaum <dtenenbaum@systemsbiology.org>
2010-03-31 12:38:28 -0700

    more toggle options, mouseover descriptions

--- js/cellphone.js
+++ js/cellphone.js
@@ -36,7 +36,7 @@ function log(message) {
 }                          
 
 
-//var carriers = [];
+var carriersHash = {"att": "AT&T", "cellularone": "CellularOne", "sprint": "Sprint", "tmobile": "T-Mobile", "uscellular": "US Cellular", "verizon": "Verizon Wireless"};
 
 var vis;
 
@@ -53,6 +53,16 @@ jQuery(document).ready(function() {
     
     // init
     vis = new org.cytoscapeweb.Visualization(div_id, options);
+    
+    
+    // set up listeners
+    vis.addListener("click", "edges", gotEdgeEvent);
+    vis.addListener("click", "nodes", gotNodeEvent);
+    vis.addListener("mouseover", "edges", gotEdgeEvent);
+    vis.addListener("mouseover", "nodes", gotNodeEvent);
+    vis.addListener("mouseout", "nodes", clearLegend);
+    vis.addListener("mouseout", "edges", clearLegend);
+    
     
     jQuery("#politics").click(function(){
        log("politics!");
@@ -105,14 +115,71 @@ jQuery(document).ready(function() {
 	
 	jQuery(".menuItem").click(function(){
 	    //alert("haha");
+	    var menuItem = jQuery(this).attr("id");
 	    log("menu item clicked: " + jQuery(this).attr("id"))
 		jQuery('ul.sf-menu').superfish();
+		// decide whether to further act on this menu item
+		if (menuItem.indexOf("special") == -1 && (menuItem.indexOf("knockout") > -1 || menuItem.indexOf("reactivate") > -1)) {
+		    //var setAttributeVisibility = function(attribute, value, visible, attrPresent) {
+		    var attribute;
+		    var value;
+		    var attrPresent;
+		    var visible;
+
+		    var tmp;
+		    if (menuItem.indexOf("carrier") > -1) {
+		        attribute = "carrier";
+		        tmp = menuItem.split("_");
+		        value = tmp[2];
+		        visible = (tmp[0] == "reactivate");
+		        //attrPresent = true;
+		    } else {
+		        attrPresent = (menuItem.indexOf("_with_") > -1);
+		        visible = (menuItem.indexOf("reactivate") > -1);
+		        tmp = menuItem.split("_");
+		        attribute = tmp[2];
+		    }
+		    setAttributeVisibility(attribute, value, visible, attrPresent);
+		}
 	})
 	
     
     
 }); // end of jQuery document ready function
 
+
+var gotNodeEvent = function(evt) {
+    var node = evt.target;
+    var str = "";
+    str += "Node Information:<br/>"
+    str += "Name: " + node.data['id'] + "<br/>";
+    var attrs = ["camera","email","carrier", "roaming", "hasbeencalled","hascalled"];
+    for (i = 0; i < attrs.length; i++) {
+        var attr = attrs[i];
+        if (node.data[attr] != undefined) {
+            str += attr + ": " + node.data[attr] + "<br/>"
+        }
+    }
+    str += "Visible: " + node.visible + "<br/>";
+    //todo add selected
+    
+    jQuery("#event_display").html(str);
+}
+
+var gotEdgeEvent = function(evt) {
+    var edge = evt.target;
+    var str = "";
+    str += "Edge information:<br/>"
+    str += "Source: " + edge.data['source'] + "<br/>";
+    str += "Target: " + edge.data['target'] + "<br/>";
+    str += "Visible: " + edge.visible + "<br/>";
+    jQuery("#event_display").html(str);
+    
+}
+
+var clearLegend = function(evt) {
+    jQuery("#event_display").html("");
+}
 
 
 var incident = function(node, edge) {
@@ -121,6 +188,38 @@ var incident = function(node, edge) {
     return (inc); 
 }
 
+var setAttributeVisibility = function(attribute, value, visible, attrPresent) {
+    log("in setattribute visibility, params are: attribute = " + attribute + ", value = " + value + ", visible = " + visible + ", attrPresent = " + attrPresent);
+    var nodes = vis.nodes();
+    var nodeList = [];
+    
+    if (value != undefined) {
+        log("carrier = " + carriersHash[value]);
+        log("att = " + carriersHash["att"]);
+    }
+    
+    for (i = 0; i < nodes.length; i++) {
+        var node = nodes[i];
+        if (value == undefined) {
+            // let's say we're reactivating phones without roaming
+            // attribute = roaming
+            // visible = true
+            // attrPresent = false
+            if (node.data[attribute] == ("" + attrPresent)) {
+                nodeList.push(node);
+            }
+        } else {
+            // we can ignore attrPresent
+            if (node.data[attribute] == carriersHash[value]) {
+                log ("found one!");
+                nodeList.push(node);
+            }
+        }
+
+        
+    }
+    setIncidentEdgesVisibility(nodeList, visible);
+}
 
 var setIncidentEdgesVisibility = function(nodes, visible) {
     for(i = 0; i < nodes.length; i++) {
